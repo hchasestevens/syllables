515850ed2d0dbfced942144796591178a50049a6
H. Chase Stevens <chase@chasestevens.com>
2014-10-14 23:42:58 +0100

    add main

---
+++ main.hs
@@ -0,0 +1,37 @@
+import Data.Char (isDigit)
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
+import Data.Maybe (isJust)
+import Text.Regex (mkRegex, matchRegex)
+import Control.Monad (forever)
+
+phonemes :: String -> String
+phonemes str = cs
+	where
+		(_ : cs : _) = splitOn "  " str
+
+
+numSyllables :: String -> Int
+numSyllables str = length $ filter isDigit $ phonemes str
+
+
+filterOnNumSyllables pronunciations = do
+	syllables <- getLine
+	let syllables' = read syllables :: Int
+	mapM_ putStrLn $ [word | word <- pronunciations, numSyllables word == syllables'] 
+
+
+filterOnStressSyllables pronunciations = do
+	pattern <- getLine
+	--let regex = mkRegex $ "^[^\\d]+" ++ (intercalate "[^\\d]+" $ map return pattern) ++ "[^\\d]*$"
+	--mapM_ putStrLn $ [word | word <- pronunciations, (isJust . matchRegex regex . phonemes) word]
+	mapM_ putStrLn $ [word | word <- pronunciations, pattern == filter isDigit (phonemes word)]
+
+
+main = do
+	file <- readFile "cmudict.0.7a.txt"
+	let pronunciations = filter (not . (==) ';' . head) $ lines file
+	putStrLn "Search by number of syllables (1) or syllable pattern (2)?"
+	choice <- getLine
+	let fn = if (choice == "1") then filterOnNumSyllables else filterOnStressSyllables
+	forever $ fn pronunciations
